eb52228f94f38e9b6cbb4e5effedb49ed90b4acd
Mehrez Bey <mahrezbey@gmail.com>
2024-02-17 20:41:24 +0100

    implementing first version of the  home page

--- express/express.py
+++ express/express.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("welcome_page.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
