d2197dbb1d3cb5ae7886ec6838d0c1d8b8bb8b30
mehrez bey <mahrezbey@gmail.com>
2024-05-31 23:55:17 +0100

    editing and deleting posts

--- express/routes.py
+++ express/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template,url_for, flash, redirect , request
+from flask import render_template,url_for, flash, redirect , request, abort
 from express import app,bcrypt,db
 from express.forms import SignupForm,LoginForm, UpdateAccountForm,CreatePostForm
 from express.models import User, Post
@@ -9,7 +9,10 @@ from flask_login import login_user , current_user , logout_user , login_required
 @app.route("/home")
 def home():
     if(current_user.is_authenticated):
-        return render_template('home.html' , title="Home")
+        image = url_for('static',filename='images/profile_pictures/'+ current_user.image_file)
+        posts = Post.query.order_by(Post.id.desc()).all()
+        print(posts)
+        return render_template('home.html' , title="Home", image = image, posts = posts)
     else : 
         return render_template("welcome_page.html")
 
@@ -81,10 +84,38 @@ def create_post():
         flash('Your post has been published!', 'success')
         return redirect(url_for('home'))
 
-    return render_template("create_post.html",title = "New Post", form = form , image=image)
+    return render_template("create_post.html",title = "New Post", form = form , image=image , context="create")
 
+@app.route("/post/edit/<int:post_id>",methods=['GET','POST'])
+@login_required
+def edit_post(post_id):
+    image = url_for('static',filename='images/profile_pictures/'+ current_user.image_file)
+    post = Post.query.get_or_404(post_id)
+    if(current_user != post.author):
+        abort(403)
+    form = CreatePostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('home'))
+    elif request.method == "GET" :
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template("create_post.html",title = "Edit Post", form = form , context="edit", image = image)
 
-
+@app.route("/post/delete/<int:post_id>",methods=['GET','POST'])
+@login_required
+def delete_post(post_id):
+    image = url_for('static',filename='images/profile_pictures/'+ current_user.image_file)
+    post = Post.query.get_or_404(post_id)
+    if(current_user != post.author):
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
 # The 2023_c version is:
 # from <app_name> import app, db
 # app.app_context().push()
